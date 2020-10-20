bcef73fcc11dcba4a888bf9e7e351b931e6b3be5
Franck Mukendi <franck.mukendi@gmail.com>
2020-10-20 00:51:52 -0400

    push

--- src/main/java/com/javainuse/service/EmployeeService.java
+++ src/main/java/com/javainuse/service/EmployeeService.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import com.javainuse.model.Employee;
 
-public interface EmployeeService {
+public interface
+EmployeeService {
 	void insertEmployee(Employee emp);
 	void insertEmployees(List<Employee> employees);
 	List<Employee> getAllEmployees();
